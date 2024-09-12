7f7cdc35e4abb69b53ac5f471d44498244cfd99d
X1aoyang <X1aoyang@node6.x1aoyang-216859.adslremotememory-pg0.clemson.cloudlab.us>
2024-09-12 02:12:39 -0400

    udpate

--- source/computing_node.cc
+++ source/computing_node.cc
@@ -183,7 +183,7 @@ void ComputingNode::woker(int proc) {
                     uint64_t shm_offset = cpu_cache_->bitmap_malloc(bin_size);
                     *(uint64_t*)((mr_rdma_addr*)cpu_cache_->buffer_[proc].buffer_+1) = shm_offset;
                     bitmap_record record = {shm_offset, bin_size};
-                    offset_record_[remote_addr] = record;
+                    //offset_record_[remote_addr] = record;
                 }
                 cpu_cache_->buffer_[proc].opcode_ = LegoOpcode::LegoIdle;
                 break;
@@ -339,8 +339,8 @@ bool ComputingNode::start(std::string* addr, std::string* port, uint32_t node_nu
         pthread_t listening_thread;
         // pthread_create(&cache_fill_thread_, NULL, run_cache_filler, this);
         // pthread_create(&recycle_thread_, NULL, run_recycler, this);   
-        worker_param new_param[128];
-        for(int i = 0; i < 72; i++) {
+        worker_param new_param[nprocs];
+        for(int i = 0; i < nprocs; i++) {
             new_param[i].heap = this; new_param[i].id = i;
             pthread_create(&woker_thread_[i], NULL, run_woker_thread, &new_param[i]);
             // sleep(1);
