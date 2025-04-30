5fd57f6436b999b99432f192cb2400cecc5d46ba
X1aoyang <X1aoyang@localhost>
2025-04-30 03:41:08 -0600

    update

--- microbench/v_microbench.cc
+++ microbench/v_microbench.cc
@@ -887,7 +887,7 @@ void* worker(void* arg) {
         break;
     }
     pthread_barrier_wait(&end_barrier);
-    if(thread_id == 0) {
+    if(thread_id == 1) {
         redis_reply = (redisReply*)redisCommand(redis_conn, "SET bench_start 0");
         freeReplyObject(redis_reply);
     }
