84ce71c82234d34dce5fc73f764deefe6f4ec134
X1aoyang <X1aoyang@localhost>
2025-04-29 22:39:53 -0600

    update install

--- include/memory_node.h
+++ include/memory_node.h
@@ -149,7 +149,7 @@ private:
     uint32_t simple_cache_rkey[32];
     uint64_t simple_cache_watermark;
 
-    bool use_global_rkey = true;
+    bool use_global_rkey = false;
     // << function enabled >>
     bool one_sided_enabled_;
     ibv_qp* rebinder_qp; ibv_cq* rebinder_cq;
--- scripts/install_ofed.sh
+++ scripts/install_ofed.sh
@@ -2,23 +2,23 @@
 
 #xl170
 
-sudo apt update
+# sudo apt update
 
-sudo apt install -y cmake libboost-all-dev libgoogle-perftools-dev libtbb-dev libgtest-dev libgflags-dev libgoogle-glog-dev
+# sudo apt install -y cmake libboost-all-dev libgoogle-perftools-dev libtbb-dev libgtest-dev libgflags-dev libgoogle-glog-dev
 
-git clone https://github.com/memkind/memkind.git
+# git clone https://github.com/memkind/memkind.git
 
-cd memkind
+# cd memkind
 
-./autogen.sh
+# ./autogen.sh
 
-./configure --prefix=/usr
+# ./configure --prefix=/usr
 
-make
+# make
 
-sudo make install
+# sudo make install
 
-cd ..; rm -rf memkind
+# cd ..; rm -rf memkind
 
 wget https://content.mellanox.com/ofed/MLNX_OFED-4.9-7.1.0.0/MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu20.04-x86_64.tgz
 
