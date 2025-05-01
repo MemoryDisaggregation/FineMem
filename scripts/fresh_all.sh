b174e2155b46a3a6becbe69e0f1c0dc3b1a6c7c4
X1aoyang <X1aoyang@localhost>
2025-04-30 20:25:50 -0600

    update

--- scripts/fresh_all.sh
+++ scripts/fresh_all.sh
@@ -3,3 +3,7 @@
 ./stop_remote_client.sh 0 17 >/dev/null 2>&1
 
 ./stop_server.sh >/dev/null 2>&1
+
+./start_server.sh >/dev/null 2>&1
+
+./stop_server.sh >/dev/null 2>&1
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,6 +2,7 @@
 
 redis-cli -h 10.10.1.1 -p 2222 SET bench_start 0 >/dev/null 2>&1
 redis-cli -h 10.10.1.1 -p 2222 SET avg 0 >/dev/null 2>&1
+redis-cli -h 10.10.1.1 -p 2222 SET time 0 >/dev/null 2>&1
 redis-cli -h 10.10.1.1 -p 2222 SET finished 0 >/dev/null 2>&1
 
 ps aux | grep "redis-server 10.10.1.1:2222" | awk '{print $2}' | xargs kill
