d86c6c3e64af54385d939831fc84be0ec17c57d8
X1aoyang <X1aoyang@localhost>
2025-05-09 07:24:19 -0600

    update

--- scripts/env_setup_xl170_batch.sh
+++ scripts/env_setup_xl170_batch.sh
@@ -2,6 +2,6 @@
 
 for i in $(seq 1 17) 
 do
-    ssh X1aoyang@node$i "sudo ~/FineMem/scripts/env_setup_xl170.sh >/dev/null 2>&1 &"
+    ssh X1aoyang@node$i "git clone https://github.com/MemoryDisaggregation/FineMem; cd ./FineMem/scripts/; sudo ./env_setup_xl170.sh >/dev/null 2>&1 &"
 done
 
