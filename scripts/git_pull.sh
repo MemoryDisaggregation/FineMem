29370c27943eeb0bd74d5f51d3fe076acc788e46
X1aoyang <X1aoyang@node0.x1aoyang-259273.adslremotememory-pg0.utah.cloudlab.us>
2025-06-05 01:26:56 -0600

    update

--- scripts/git_pull.sh
+++ scripts/git_pull.sh
@@ -2,5 +2,5 @@
 
 for i in $(seq $1 $2) 
 do
-    ssh X1aoyang@node$i "cd ~/FineMem/build; git stash; git pull; cmake ..; make -j"
+    ssh X1aoyang@node$i "mkdir ~/FineMem/build; cd ~/FineMem/build; git stash; git checkout revision; git pull; cmake ..; make -j"
 done
